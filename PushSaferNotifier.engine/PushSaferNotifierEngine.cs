ed1e087f8b5bde32a0d6e4551eb89401550ad384
Mattis Ovesen Aamodt <mattis@visual-mind.com>
2018-12-26 09:53:20 +0100

    Added notification controller & engine

---
+++ PushSaferNotifier.engine/PushSaferNotifierEngine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PushSaferNotifier.engine
+{
+	public class PushSaferNotifierEngine
+	{
+		public static string[] Notify()
+		{
+			return new string[] { "Notified!", "Yup!" };
+		}
+	}
+}
---
+++ PushSaferNotifier.web/Controllers/NotificationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PushSaferNotifier.engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PushSaferNotifier.web.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class NotificationController : ControllerBase
+	{
+		// GET api/notification
+		[HttpGet]
+		public ActionResult<IEnumerable<string>> Get()
+		{
+			string[] v = PushSaferNotifierEngine.Notify();
+			return v;
+		}
+	}
+}
